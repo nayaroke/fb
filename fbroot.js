a78adcc544c8d5df5d082f02a18fe6bcbcdfcc79
nayaroke <ahmadnayar99@gmail.com>
2017-02-13 08:04:43 +0700

    Add files via upload

---
+++ fbroot.js
@@ -0,0 +1,12 @@
+(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;js.async=true;
+js.src = "//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.0";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+function downloadJSAtOnload() {
+        var e = document.createElement("script");
+        e.src = "//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.7", document.body.appendChild(e)
+    }
+    window.addEventListener ? window.addEventListener("load", downloadJSAtOnload, !1) : window.attachEvent ? window.attachEvent("onload", downloadJSAtOnload) : window.onload = downloadJSAtOnload;
